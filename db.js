f639cf1775a5fa8348892a2388ccbaa01be627b8
pmanonline <petersonzoconis@gmail.com>
2023-07-26 04:50:46 +0100

    change urls

--- db.js
+++ db.js
@@ -1,10 +1,21 @@
 const mysql = require("mysql2");
 
+// const db = mysql.createPool({
+//   user: "root",
+//   host: "localhost",
+//   password: "w3schools.com",
+//   database: "blogpost",
+// });
+
+// module.exports = db;
+
 const db = mysql.createPool({
-  user: "root",
-  host: "localhost",
-  password: "w3schools.com",
+  user: "admin",
+  host: "mysql-137535-0.cloudclusters.net",
+  password: "Lsk8f1Ay",
   database: "blogpost",
+  IPAddress: "108.181.197.182",
+  port: "10041",
 });
 
 module.exports = db;

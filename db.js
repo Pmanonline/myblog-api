84af124484487df1de950bd79a160b8d6a19464b
pmanonline <petersonzoconis@gmail.com>
2023-08-05 18:31:43 +0100

    configured database

--- db.js
+++ db.js
@@ -1,12 +1,27 @@
+// const mysql = require("mysql2");
+
+// const db = mysql.createPool({
+//   user: "admin",
+//   host: "mysql-137535-0.cloudclusters.net",
+//   password: "Lsk8f1Ay",
+//   database: "blogpost",
+//   IPAddress: "108.181.197.182",
+//   port: "10041",
+// });
+
+// module.exports = db;
+
 const mysql = require("mysql2");
 
 const db = mysql.createPool({
-  user: "admin",
-  host: "mysql-137535-0.cloudclusters.net",
-  password: "Lsk8f1Ay",
-  database: "blogpost",
-  IPAddress: "108.181.197.182",
-  port: "10041",
+  user: "root",
+  host: "containers-us-west-131.railway.app",
+  password: "DA2v82DifoLmL9bh2QA1",
+  database: "railway",
+  url: "mysql://root:DA2v82DifoLmL9bh2QA1@containers-us-west-131.railway.app:5860/railway",
+  //   database: "blogpost",
+  // IPAddress: "108.181.197.182",
+  port: "5860",
 });
 
 module.exports = db;
